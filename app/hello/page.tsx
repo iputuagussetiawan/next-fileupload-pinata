cc6a540a5091ae980b41683579c520a3dfb82b50
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-11-27 18:29:25 +0800

    optimize image

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -6,7 +6,12 @@ async function getData() {
     const url=await pinata.gateways.createSignedURL({
         cid:'bafkreihwaqcvws64jfkwgn62zuj65ckssnydwi6y7dlfgrpqs65df5ljaa',
         expires:3600,
-    })
+    }).optimizeImage({
+        width:500,
+        height:500,
+        format:'webp',
+        quality:80,
+    });
     return url;
 }
 
